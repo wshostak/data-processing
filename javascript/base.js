082a0bb1dcaeb6804d9656aa0b907918e1e5d654
William Shostak <william.shostak@gmail.com>
2017-04-10 00:49:34 -0700

    testing functions

--- javascript/base.js
+++ javascript/base.js
@@ -1,70 +1,334 @@
 window.resizeTo(Math.round(screen.width * .8), Math.round(screen.height * .8));
 
-window.data = [];
-
-document.addEventListener("upload-success", fileUploaded);
-
-function fileUploaded(e) {
-
-  var file_text = e.detail.file.text,
-      file_name = e.detail.file.name,
-      file_ext = file_name.split(".")[1].toLowerCase(),
-      ten_lines = file_text.split(/\n|\r/, 10).join("\n"),
-      delimiter = guessDelimiter(ten_lines)[0],
-      dsv = d3.dsvFormat(delimiter),
-      data = [];
-
-  if (file_ext == "json") {
-
-  } else {
-
-    data = dsv.parse(file_text);
-    changePage();
+window.DataProcessing = window.DP = {
+  excel: {
+    files: []
   }
-
-  window.data = data;
-console.log(data[0]);
-console.log(data[1]);
 };
 
-function changePage(page) {
+window.scriptTimer = function (id, clear) {
 
-console.log("here")
-  var next_page = {
-    upload: "parser",
-    parser: "output",
-    output: "upload",
+  var now = performance.now();
+
+  clear = clear || false;
+  id = id || "main";
+
+  if (typeof window.scriptTimer === "undefined") {
+
+    window.scriptTimer = {};
   }
 
-  page = page || next_page[window.location.hash.replace(/[^a-z0-9_-]*/gi, '')] || "parser";
+  if (typeof window.scriptTimer[id] !== "undefined") {
 
-  window.history.pushState({}, null, '/#/' + page);
-  window.dispatchEvent(new CustomEvent('location-changed'));  
+    var then = window.scriptTimer[id];
+
+    if (clear) {
+      
+      window.scriptTimer[id] = now;
+    }
+
+    return now - then;
+  }
+
+  window.scriptTimer[id] = now;
 }
 
-function guessDelimiter (text) {
+String.prototype.toObject = function () {
 
-  var possibleDelimiters = text.split('').filter(function(item, i, ar) {
-    return ar.indexOf(item) === i;
-  }).join('').replace(/[a-z0-9]*/gi, '');
+  try {
 
-  return possibleDelimiters.split('').filter(function(delimiter) {
+    var obj_str = this.toString(),
+        obj = window;
 
-    var cache = -1;
+    obj_str = obj_str.split('#')[0].split('.');
 
-    return text.split('\n').every(function(line) {
+    for (var i = 0; i < obj_str.length; i++) obj = obj[obj_str[i]];
 
-      if (!line) {
-        return true;
-      }
+    return obj;
+  } catch (e) {return false;}
+};
 
-      var length = line.split(delimiter).length;
+String.prototype.toKey = function () {
 
-      if (cache < 0) {
-        cache = length;
-      }
+  return this.toString().split('#')[1] || undefined;
+};
 
-      return cache === length && length > 1;
+String.prototype.dashedCase = function () {
+
+  return this.toString().replace(/(.[A-Z])/g, function (all, letters) {
+
+  	return letters[0] + '-' + letters[1].toLowerCase();
+	});
+};
+
+String.prototype.unCamelCase = function (charcter, titlelize) {
+
+  return this.toString().replace(/(.[A-Z])/g, function (all, letters) {
+
+  	return letters[0] + (charcter || '-') + letters[1].toLowerCase().replace(/\w\S*/g, function (text) {
+
+    	return titlelize? text.charAt(0).toUpperCase() + text.substr(1).toLowerCase(): text;
     });
+	});
+};
+
+String.prototype.camelCase = function () {
+
+  return this.toString().replace(/([ _\-][A-Z])/gi, function (all, letters) {
+
+  	return letters[1].toUpperCase();
+	});
+};
+
+String.prototype.unserialize = function () {
+
+	var str = decodeURI(this),
+	    pairs = str.split('&'),
+	    obj = {}, p, idx, ind;
+
+	for (var i = 0; i < pairs.length; i++) {
+
+		p = pairs[i].split('=');
+		idx = p[0];
+
+		if (idx.indexOf("[]") == (idx.length - 2)) {
+
+			ind = idx.substring(0, idx.length - 2)
+
+			if (obj[ind] === undefined) obj[ind] = [];
+
+			obj[ind].push(p[1]);
+		} else {
+
+			obj[idx] = p[1];
+		}
+	}
+
+	return obj;
+};
+
+String.prototype.isBoolean = function () {
+
+  try {
+
+    switch (this.trim().toLowerCase()) {
+
+      case "true":
+      case "yes":
+      case "y":
+      case true:
+        return true;
+    }
+  } catch (e) {}
+
+  return false;
+};
+
+String.prototype.toBoolean = function () {
+
+  try {
+
+    switch (this.trim().toLowerCase()) {
+
+      case "true":
+      case "yes":
+      case "y":
+      case true:
+        return true;
+
+      case "false":
+      case "no":
+      case "n":
+      case false:
+      case null:
+      case undefined:
+      case NaN:
+        return false;
+    }
+  } catch (e) {}
+
+  return false;
+};
+
+String.prototype.isNumber = function () {
+
+  try {
+
+    var str = this.trim().replace(/^0+(?=\d)/, '').replace(/\.?0+$/,""),
+        int = parseInt(str),
+        float = parseFloat(str);
+
+    if (int + '' === str) return true;
+    if (float + '' === str) return true;
+  } catch (e) {}
+
+  return false;
+};
+
+String.prototype.toNumber = function () {
+
+  try {
+
+    var str = this.trim().replace(/^0+(?=\d)/, '').replace(/\.?0+$/,""),
+        int = parseInt(str),
+        float = parseFloat(str);
+
+    if (int + '' == str) return int;
+    if (float + '' == str) return float;
+  } catch (e) {}
+
+  return 0;
+};
+
+String.prototype.isType = function () {
+
+  if (this.isBoolean()) return "boolean";
+  if (this.isNumber()) return "number";
+
+  return "string";
+};
+
+// ToDo: make own page in app, allow uploading of files.
+var cell_references_regex = /(['][^']+['][!]([$A-Z]+[$0-9]+|[$A-Z]+:[$A-Z]+)|([$A-Z]+[$0-9]+|[$A-Z]+:[$A-Z]+))/g,
+    object_references_regex = /(\[[a-zA-Z0-9 #&\?]+\]\[[a-zA-Z0-9 #&\?]+\])/g;
+
+function handleLoad (e) {
+
+  var href = decodeURIComponent(e.target.href.replace('.txt', '')).split('/'),
+      file = href[href.length -1];
+
+  DP.excel.files.push(file);
+  data = d3.tsvParse(e.path[0].import.querySelector('body').textContent);
+
+  DP.excel[file] = data;
+  data = {};
+
+  DP.excel[file].columns.forEach(function (val, index) {
+
+    data[columnToLetter(index + 1)] = val;
   });
+
+  DP.excel[file].column_to_letter = data;
+
+  if (DP.excel.files.length > 3) startDataCrunch();
+}
+
+function columnToLetter (column) {
+
+  var temp, letter = '';
+
+  while (column > 0) {
+
+    temp = (column - 1) % 26;
+    letter = String.fromCharCode(temp + 65) + letter;
+    column = (column - temp - 1) / 26;
+  }
+
+  return letter;
+}
+
+function startDataCrunch () {
+
+  DP.excel['Account&WirelessSummary'].column_to_letter.AE = DP.excel['Account&WirelessSummary'].column_to_letter.A;
+  DP.excel['Account&WirelessSummary'].column_to_letter.AF = DP.excel['Account&WirelessSummary'].column_to_letter.B;
+
+  delete DP.excel['Account&WirelessSummary'].column_to_letter.A;
+  delete DP.excel['Account&WirelessSummary'].column_to_letter.B;
+
+  DP.excel.files.forEach(eachFile);
+
+  output()
+}
+
+_.right = function (str, len) {
+
+  len = len || str.length;
+
+  return str.slice(-len)
+}
+
+_.left = function (str, len) {
+
+  len = len || str.length;
+
+  return str.slice(0, len)
+}
+
+_.pluck = function (obj, func) {
+
+  return _.first(_.values(_.pick(_.first(obj), func)));
+}
+
+_.and = function () {
+
+  return Array.from(arguments).every(function (item) {
+
+    return !_.isEmpty(item);
+  });
+}
+_.or = function () {
+
+  return _.filter(arguments, function (item) {
+
+    return !_.isEmpty(item);
+  });
+};
+
+function eachFile (file) {
+
+//console.error("file: " + file);
+  for (var property in DP.excel[file][0]) {
+
+    if (DP.excel[file][0].hasOwnProperty(property) && DP.excel[file][0][property] && DP.excel[file][0][property][0] === '=') {
+
+//console.debug("property: " + property);
+      DP.excel[file][0][property] = replaceProperties(file, DP.excel[file], property);
+
+      var property_str = DP.excel[file][0][property];
+
+      DP.excel[file][0][property] = excelFormulaUtilities.formatFormula(DP.excel[file][0][property]).replace(/"\{/g, '{').replace(/\}"/g, '}').replace(/\}\s*(\/|\+|\-)\s*\{/g, '},"$1",{').replace(/\}\s*(=|\<>|\-)\s*([^"]*)"/g, ',"$1": "$2"}');
+
+      try {
+        
+        try {
+
+          DP.excel[file][0][property] = JSON.parse(DP.excel[file][0][property]);
+        } catch (e) {
+
+          DP.excel[file][0][property] = JSON.parse('[' + DP.excel[file][0][property] + ']');
+        }
+      } catch (e) {
+        
+        DP.excel[file][0][property] = property_str.replace('=', '');
+      }
+//console.log(DP.excel[file][0][property]);
+    }
+  }
+}
+
+function output () {
+
+//console.log(DP.excel);
+console.log(JSON.stringify(DP.excel).replace(/\\"/g, "'"));
+}
+
+function replaceProperties (file, obj, property) {
+
+  return obj[0][property].replace(cell_references_regex, function (cell_reference) {
+          
+    var cell_reference_array = cell_reference.replace(/['$]/g, '').split('!'),
+        columns = cell_reference_array.pop().replace(/[^A-Z:]/g, '').split(':'),
+        column_one = columns.shift(),
+        column_two = columns.shift() || false,
+        sheet = cell_reference_array.pop() || file,
+        column_one_output = columnOutpuStr(column_one, sheet),
+        output_str = (column_two && column_two !== column_one)? column_one_output + ':' + columnOutpuStr(column_two, sheet): column_one_output; 
+
+    return output_str;
+  });
+}
+
+function columnOutpuStr (column, sheet) {
+
+  return "@'" + sheet + "." + (DP.excel[sheet].column_to_letter[column] || column) + "'";
 }
